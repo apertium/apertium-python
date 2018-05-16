f1d47bf61db974836ed0673c4b1c3553ac9ab93d
arghyatiger <arghyatiger@gmail.com>
2018-05-16 12:44:15 +0530

    tests update

--- analysis/__init__.py
+++
@@ -1,4 +0,0 @@
-import analysis.main
-import analysis.modesearch
-import analysis.translation
-import analysis.utils
--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,22 @@
-import os
 import sys
+import os
 import unittest
 
 base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 sys.path.append(base_path)
 
-import analysis
-import analysis.modesearch
 from analysis.main import Analyzer
 
-
 a = Analyzer('../langdata/apertium-eng/')
- 
+
+
 class TestUM(unittest.TestCase):
 
     def test_en(self):
-        self.assertEqual(str(Analyzer.analyze(a, 'cats', 'en')), "cats/cat<n><pl>")
+        self.assertEqual(str(Analyzer.analyze(a, 'cats', 'en')), 'cats/cat<n><pl>')
+        self.assertEqual(str(Analyzer.analyze(a, 'dogs', 'en')), 'dogs/dog<n><pl>')
 
- 
+
+
 if __name__ == '__main__':
     unittest.main()
