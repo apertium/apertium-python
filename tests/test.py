a66e456251379f746aee510dead977403aa7ddbd
arghyatiger <arghyatiger@gmail.com>
2018-05-16 13:49:31 +0530

    tests update

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,11 @@ import unittest
 base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 sys.path.append(base_path)
 
-from analysis.main import Analyzer
+try:
+    from analysis.main import Analyzer
+except Exception:
+    print('Error in import')
+    raise
 
 a = Analyzer('../langdata/apertium-eng/')
 
@@ -15,7 +19,10 @@ class TestUM(unittest.TestCase):
     def test_en(self):
         self.assertEqual(str(Analyzer.analyze(a, 'cats', 'en')), 'cats/cat<n><pl>')
         self.assertEqual(str(Analyzer.analyze(a, 'dogs', 'en')), 'dogs/dog<n><pl>')
-
+        self.assertEqual(str(Analyzer.analyze(a, 'women', 'en')), 'women/woman<n><pl>')
+        self.assertEqual(str(Analyzer.analyze(a, 'men', 'en')), 'men/man<n><pl>')
+        self.assertEqual(str(Analyzer.analyze(a, 'books', 'en')), 'books/book<n><pl>/book<vblex><pres><p3><sg>')
+        self.assertEqual(str(Analyzer.analyze(a, 'luggages', 'en')), 'luggages/luggage<n><pl>')
 
 
 if __name__ == '__main__':
