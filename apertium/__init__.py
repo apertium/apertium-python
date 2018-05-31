35747d131bb61c6bcc0ef5ea6f57a9e3d1a43874
arghyatiger <arghyatiger@gmail.com>
2018-05-31 12:55:03 +0530

    changes for the translation module

--- apertium/__init__.py
+++ apertium/__init__.py
@@ -1,6 +1,7 @@
 from apertium.mode_search import search_path
 from apertium.analysis import analyze  # noqa: F401
 from apertium.generation import generate  # noqa: F401
+from apertium.translation import translate  #noqa: F401
 
 if False:
     from typing import List, Dict, Tuple  # noqa: F401
@@ -9,9 +10,14 @@ if False:
 class ModeNotInstalled(ValueError):
     pass
 
+class PairNotInstalled()
+
 
 def update_modes(pair_path):  # type: (str) -> None
     modes = search_path(pair_path)
+    if modes['pair']
+        for path, lang_src, lang_trg in modes['pair']:
+            pairs['%s-%s' % (lang_src, lang_trg)] = path
     if modes['analyzer']:
         for dirpath, modename, lang_pair in modes['analyzer']:
             analyzers[lang_pair] = (dirpath, modename)
@@ -28,5 +34,10 @@ def append_pair_path(pair_path):  # type: (str) -> None
 pair_paths = ['/usr/share/apertium', '/usr/local/share/apertium']
 analyzers = {}  # type: Dict[str, Tuple[str, str]]
 generators = {}  # type: Dict[str, Tuple[str, str]]
+pairs = {}
+pairs_graph = {}
+paths = {}
+pipelines = {}
+pipeline_cmds = {}
 for pair_path in pair_paths:
     update_modes(pair_path)
--- apertium/utils.py
+++ apertium/utils.py
@@ -26,3 +26,49 @@ def execute(inp, commands):  # type: (str, List[List[str]]) -> str
         )
         end, _ = procs[i].communicate(end)
     return end.decode()
+
+def start_pipeline(commands):
+    procs = []  # type: List[tornado.process.Subprocess]
+    for i, cmd in enumerate(commands):
+        if i == 0:
+            in_from = tornado.process.Subprocess.STREAM
+        else:
+            in_from = procs[-1].stdout
+        if i == len(commands) - 1:
+            out_from = tornado.process.Subprocess.STREAM
+        else:
+            out_from = PIPE
+        procs.append(tornado.process.Subprocess(cmd,
+                                                stdin=in_from,
+                                                stdout=out_from))
+    return procs[0], procs[-1]
+
+def parse_mode_file(mode_path):
+    mode_str = open(mode_path, 'r').read().strip()
+    if mode_str:
+        if 'ca-oc@aran' in mode_str:
+            do_flush = False
+            modes_parentdir = os.path.dirname(os.path.dirname(mode_path))
+            mode_name = os.path.splitext(os.path.basename(mode_path))[0]
+            commands = [[
+                'apertium',
+                '-f', 'html-noent',
+                # Get the _parent_ dir of the mode file:
+                '-d', modes_parentdir,
+                mode_name,
+            ]]
+        else:
+            do_flush = True
+            commands = []
+            for cmd in mode_str.strip().split('|'):
+                # TODO: we should make language pairs install
+                # modes.xml instead; this is brittle (what if a path
+                # has | or ' in it?)
+                cmd = cmd.replace('$2', '').replace('$1', '-g')
+                if cmd_needs_z(cmd):
+                    cmd = re.sub(r'^\s*(\S*)', r'\g<1> -z', cmd)
+                commands.append([c.strip("'")
+                                 for c in cmd.split()])
+        return ParsedModes(do_flush, commands)
+    else:
+        raise Exception('Could not parse mode file %s', mode_path)
