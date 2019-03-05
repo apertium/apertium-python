ef3672133b051ebab3728d27f9b571675edeef81
Jammie sandy <33603488+jamessandy@users.noreply.github.com>
2019-03-05 21:26:50 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='apertium',
+      version='1.0',
+      description='this is a python library for apertium',
+      author='James Sandy',
+      author_email='mondaysandy3@gmail.com',
+      url='https://github.com/apertium/apertium-python/',
+      packages=['apertium','lib'],
+     )
+
