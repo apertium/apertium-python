17a9017ed80cb2f36a55ebd9950878ecdac2ef86
Vaydheesh <lokendras1998@gmail.com>
2019-04-08 08:35:53 +0530

    Added: python setup.py test

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     install_requires=[
         'apertium-streamparser==5.0.2',
     ],
+    test_suite = 'tests',
     packages=find_packages(exclude=['tests']),
     cmdclass={
         'install': PostInstallCommand,
