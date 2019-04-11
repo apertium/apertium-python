08d5344a98c5bcbfc5a52e5a2e1975307e215e1b
Vaydheesh <lokendras1998@gmail.com>
2019-04-11 09:18:45 +0530

    Implemented changes suggested
    - Added: TODO values
    - Removed: redundant spaces around operator while calling setup()

--- setup.py
+++ setup.py
@@ -18,20 +18,16 @@ class PostInstallCommand(install):
 
 setup(
     name='apertium-python',
-    # version='',
+    # TODO: Add version description, keywords, author, author_email
     license='GNU General Public License v3.0 ',
-    # description='',
     long_description=open(path.join(path.abspath(path.dirname(__file__)), 'README.md')).read(),
     long_description_content_type='text/markdown; charset=UTF-8',
-    # keywords='',
-    # author='',
-    # author_email='',
     url='https://github.com/apertium/apertium-python',
     python_requires='>=3.4',
     install_requires=[
         'apertium-streamparser==5.0.2',
     ],
-    test_suite = 'tests',
+    test_suite='tests',
     packages=find_packages(exclude=['tests']),
     cmdclass={
         'install': PostInstallCommand,
