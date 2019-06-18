74fcc51aacc1749c6272ab2f483ed9584ff0605d
Sushain Cherivirala <sushain@skc.name>
2019-06-18 13:33:55 -0500

    Cleanup swig build script.

--- build-swig-wrapper.sh
+++ build-swig-wrapper.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+set -e
+
 sudo apt-get install -y swig build-essential python3-setuptools
 git clone -b swig_wrapper https://github.com/Vaydheesh/lttoolbox.git
-cd lttoolbox || set -e
+cd lttoolbox
 ./autogen.sh --enable-python-bindings && make
-cd python || set -e
+cd python
 python3 setup.py install
