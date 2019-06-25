d4fcf2ad6ce57be3b0cf3a892cd1ec2e92963c2e
Lokendra Singh <lokendras1998@gmail.com>
2019-06-26 02:43:05 +0530

    Wrapper for lrx-proc -m (#46)
    * Added: Wrapper for lrx-proc -m
    
    * Implemented changes suggested
    
    build-swig-wrapper: clone lttoolbox for building wrapper
    utils.py: Better name for flags

--- build-swig-wrapper.sh
+++ build-swig-wrapper.sh
@@ -2,8 +2,14 @@
 set -e
 
 sudo apt-get install -y swig build-essential python3-setuptools
-git clone -b swig_wrapper https://github.com/Vaydheesh/lttoolbox.git
+git clone https://github.com/apertium/lttoolbox.git
 cd lttoolbox
+./autogen.sh --enable-python-bindings && make
+cd python
+python3 setup.py install
+
+git clone -b swig_wrapper https://github.com/Vaydheesh/apertium-lex-tools.git
+cd apertium-lex-tools
 ./autogen.sh --enable-python-bindings && make
 cd python
 python3 setup.py install
