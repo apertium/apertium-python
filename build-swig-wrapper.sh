55b9418ed87eb4b27d766a7d6dab60ce8e5f73a7
Sushain Cherivirala <sushain@skc.name>
2019-07-11 11:35:57 -0500

    Don't install wrappers inside of each other

--- build-swig-wrapper.sh
+++ build-swig-wrapper.sh
@@ -4,19 +4,22 @@ set -xe
 sudo apt-get install -y swig build-essential python3-setuptools
 
 git clone --depth 1 https://github.com/apertium/lttoolbox.git
-cd lttoolbox
+pushd lttoolbox
 ./autogen.sh --enable-python-bindings && make -j2
 cd python
 python3 setup.py install
+popd
 
 git clone --depth 1 https://github.com/apertium/apertium-lex-tools.git
-cd apertium-lex-tools
+pushd apertium-lex-tools
 ./autogen.sh --enable-python-bindings && make -j2
 cd python
 python3 setup.py install
+popd
 
 git clone --depth 1 https://github.com/apertium/apertium.git
-cd apertium
+pushd apertium
 ./autogen.sh --enable-python-bindings && make -j2
 cd python
 python3 setup.py install
+popd
