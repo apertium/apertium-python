edc81bb43f0b68cb2894c1208f74a3dafb049f52
vaydheesh <lokendras1998@gmail.com>
2019-07-20 11:15:47 +0530

    dont build lttoolbox

--- build-swig-wrapper.sh
+++ build-swig-wrapper.sh
@@ -3,14 +3,6 @@ set -xe
 
 sudo apt-get install -y swig build-essential python3-setuptools
 
-git clone --depth 1 https://github.com/apertium/lttoolbox.git
-pushd lttoolbox
-./autogen.sh --enable-python-bindings
-cd python
-make -j2
-python3 setup.py install
-popd
-
 git clone --depth 1 https://github.com/apertium/apertium-lex-tools.git
 pushd apertium-lex-tools
 ./autogen.sh --enable-python-bindings
