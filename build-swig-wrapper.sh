1327f6407924a036d53db3456ca4237397ff4ed9
vaydheesh <lokendras1998@gmail.com>
2019-08-01 22:17:31 +0530

    clone apertium master, branch merged

--- build-swig-wrapper.sh
+++ build-swig-wrapper.sh
@@ -11,7 +11,7 @@ make -j2
 python3 setup.py install
 popd
 
-git clone --depth 1 -b wrapper_optimise https://github.com/apertium/apertium.git apertium-core
+git clone --depth 1 https://github.com/apertium/apertium.git apertium-core
 pushd apertium-core
 ./autogen.sh --enable-python-bindings
 cd python
