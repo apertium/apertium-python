c4d17097fc858e047c45563b667240c83b46d960
vaydheesh <lokendras1998@gmail.com>
2019-11-01 00:55:31 +0530

    bump version to 0.2.3

--- apertium/__init__.py
+++ apertium/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Lokendra Singh, Arghya Bhatttacharya, Sushain K. Cherivirala, Andi Qu'
 __license__ = 'GNU General Public License v3.0'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 import logging
 import os
