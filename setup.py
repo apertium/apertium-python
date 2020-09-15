0c3785e35b870668a270ab51f41891923ab104c2
vaydheesh <lokendras1998@gmail.com>
2020-09-15 11:45:04 +0530

    rename: install language en-es -> eng-spa

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def install_binaries() -> None:
 
     apertium.installer.install_apertium()
     apertium.installer.install_module('eng')
-    apertium.installer.install_module('en-es')
+    apertium.installer.install_module('eng-spa')
     apertium.installer.install_module('kaz-tat')
     apertium.installer.install_wrapper('python3-apertium-core')
     apertium.installer.install_wrapper('python3-apertium-lex-tools')
