d634c209e26a4237fbea3b4ea789efbffb65d9e7
vaydheesh <lokendras1998@gmail.com>
2020-09-17 01:51:40 +0530

    remove: install_apertium_kaz_tat_stable.sh

--- tests/install_apertium_kaz_tat_stable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Replace kaz-tat.automorf.bin with stable version
-wget 'https://apertium.projectjj.com/win32/release/data.php?deb=apertium-kaz-tat' -O apertium-kaz-tat.deb
-ar -x apertium-kaz-tat.deb && tar -xvf data.tar.xz
-sudo mv -f ./usr/share/apertium/apertium-kaz-tat/kaz-tat.automorf.bin /usr/share/apertium/apertium-kaz-tat/kaz-tat.automorf.bin
